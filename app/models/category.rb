2a52233d4ae85ee9726ec99cc64596787b90d1ca
vladaross <vladaross@icloud.com>
2018-08-02 20:51:50 +0300

    Added validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :items
+  validates :name, length: { in: 2..20 }
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
   has_and_belongs_to_many :categories
+  validates :name, length: { in: 2..20 }
+  validates :price, presence: true
 end
