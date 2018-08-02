d24be2c4647da8554067fdbbd075db9b54640227
vladaross <vladaross@icloud.com>
2018-08-02 20:55:26 +0300

    Added validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :items
   validates :name, length: { in: 2..20 }
+  alias_attribute :label, :name
+  validates :label, presence: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
   has_and_belongs_to_many :categories
   validates :name, length: { in: 2..20 }
   validates :price, presence: true
+  alias_attribute :label, :name
+  validates :label, presence: true
 end
