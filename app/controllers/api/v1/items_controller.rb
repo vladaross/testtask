368f786f5debd450f1096da8aff007f6f289b18b
vladaross <vladaross@icloud.com>
2018-08-03 11:59:20 +0300

    dropped decimal

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,45 @@
+class Api::V1::ItemsController < ApplicationController
+  before_action :set_items, only: [:show, :update, :destroy]
+  
+  # GET /todos
+  def index
+    @items = Item.all.page params[:page]
+    json_response(@items)
+  end
+
+  # POST /items
+  def create
+    @items = Item.create!(items_params)
+    json_response(@items, :created)
+  end
+
+  # GET /items/:id
+  def show
+    @item = Item.find_by_slug(params[:id])
+    json_response(@items)
+  end
+
+
+  # PUT /items/:id
+  def update
+    @items.update(items_params)
+    head :no_content
+  end
+
+  # DELETE /items/:id
+  def destroy
+    @items.destroy
+    head :no_content
+  end
+
+  private
+
+  def items_params
+    # whitelist params
+    params.permit(:id, :name, :price, :title, :created_by)
+  end
+
+  def set_items
+    @items = Item.find_by_slug(params[:id])
+  end
+end
